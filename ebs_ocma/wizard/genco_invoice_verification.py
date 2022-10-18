9d2f1fd9c379b1ca32c24bf8cb8cdb7a2dca152e
Steve Olise <solise@mceesolutions.com>
2022-10-18 14:12:06 +0100

    [ADD]modules

--- ebs_ocma/wizard/genco_invoice_verification.py
+++ ebs_ocma/wizard/genco_invoice_verification.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, _
+from odoo import models, fields, api, _
 
 class CreateTransfer(models.TransientModel):
     _name = 'ebs_ocma.genco.invoice.transfer'
@@ -15,13 +15,79 @@ class CreateTransfer(models.TransientModel):
                 return billing_cycle and billing_cycle.id or False
         return False
 
+    @api.onchange('billing_cycle_id')
+    def _onchange_billing_cycle_id(self):
+        verification_lines = self.env['ebs_ocma.genco.invoice.transfer.lines'].sudo()
+        self.verification_line_ids.sudo().unlink()
+        verification_line_ids = self.verification_line_ids
+        if self.billing_cycle_id:
+            for computed_line in self.bc_genco_parameter_ids:
+                for genco_line in self.nbet_genco_parameter_ids:
+                    if computed_line.partner_id == genco_line.partner_id:
+                        verification_line_ids = verification_lines.create(
+                            {
+                                'partner_id': computed_line.partner_id and computed_line.partner_id.id,
+                                'nbet_capacity_payment': computed_line.capacity_payment,
+                                'nbet_energy_payment': computed_line.energy_payment,
+                                'nbet_total_payment': computed_line.total_payment,
+
+                                'genco_capacity_payment': genco_line.capacity_payment,
+                                'genco_energy_payment': genco_line.energy_payment,
+                                'genco_total_payment': genco_line.total_payment,
+                                'verification_id': self.id,
+                            }
+                        )
+
     billing_cycle_id = fields.Many2one(comodel_name='billing.cycle', string="Billing Cycle", default=_get_billing_cycle_id, readonly=True)
+
     bc_genco_parameter_ids = fields.One2many(
         comodel_name="ebs_ocma.genco.invoice.billing_cycle.parameter", inverse_name="billing_cycle_id", string="Genco Parameters", related='billing_cycle_id.bc_genco_parameter_ids', readonly=False)
 
     nbet_genco_parameter_ids = fields.One2many(
         comodel_name="ebs_ocma.nbet.genco.invoice.billing_cycle", inverse_name="billing_cycle_id", string="NBET Genco Parameters", related='billing_cycle_id.nbet_genco_parameter_ids', readonly=False)
 
+    verification_line_ids = fields.One2many(
+        comodel_name="ebs_ocma.genco.invoice.transfer.lines", inverse_name="verification_id", string="Verification Lines", readonly=False)
+
     def verify_invoice(self):
         for rec in self:
             return
+
+    def select_all_nbet(self):
+        for rec in self:
+            for line in rec.verification_line_ids:
+                line.write({
+                    'nbet_verified': True
+                }) 
+        return True
+
+class CreateTransfer(models.TransientModel):
+    _name = 'ebs_ocma.genco.invoice.transfer.lines'
+
+    verification_id = fields.Many2one(comodel_name='ebs_ocma.genco.invoice.transfer', string='Genco')
+
+    partner_id = fields.Many2one(comodel_name='res.partner', string='Genco')
+
+    nbet_verified = fields.Boolean(string='NBET verified invoice')
+    nbet_capacity_payment = fields.Float('NBET Capacity Payment')
+    nbet_energy_payment = fields.Float('NBET Energy Payment')
+    nbet_total_payment = fields.Float('NBET Total Payment')
+
+    genco_verified = fields.Boolean(string='GENCO verified invoice')
+    genco_capacity_payment = fields.Float('GENCO Capacity Payment')
+    genco_energy_payment = fields.Float('GENCO Energy Payment')
+    genco_total_payment = fields.Float('GENCO Total Payment')
+
+    remarks = fields.Char(string='Comments')
+
+    @api.onchange('nbet_verified')
+    def _onchange_nbet_verified(self):
+        for rec in self:
+            if rec.nbet_verified == True:
+                rec.genco_verified = False
+    
+    @api.onchange('genco_verified')
+    def _onchange_genco_verified(self):
+        for rec in self:
+            if rec.genco_verified == True:
+                rec.nbet_verified = False
