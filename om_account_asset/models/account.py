375317532f407271024f9d041a016d9c60131964
Dominic <dominic.anyanna@gmail.com>
2022-11-03 16:44:05 +0100

    asset split

--- om_account_asset/models/account.py
+++ om_account_asset/models/account.py
@@ -7,7 +7,8 @@ from odoo import api, fields, models
 class AccountMove(models.Model):
     _inherit = 'account.move'
 
-    asset_depreciation_ids = fields.One2many('account.asset.depreciation.line', 'move_id', string='Assets Depreciation Lines', ondelete="restrict")
+    asset_depreciation_ids = fields.One2many('account.asset.depreciation.line', 'move_id',
+                                             string='Assets Depreciation Lines')
 
     def button_cancel(self):
         for move in self:
@@ -15,8 +16,8 @@ class AccountMove(models.Model):
                 line.move_posted_check = False
         return super(AccountMove, self).button_cancel()
 
-    def post(self):
+    def action_post(self):
         for move in self:
             for depreciation_line in move.asset_depreciation_ids:
                 depreciation_line.post_lines_and_close_asset()
-        return super(AccountMove, self).post()
+        return super(AccountMove, self).action_post()
--- om_account_asset/wizard/asset_modify.py
+++ om_account_asset/wizard/asset_modify.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
 
-
 from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 
 class AssetModify(models.TransientModel):
@@ -14,7 +14,6 @@ class AssetModify(models.TransientModel):
     method_period = fields.Integer(string='Period Length')
     method_end = fields.Date(string='Ending date')
     asset_method_time = fields.Char(compute='_get_asset_method_time', string='Asset Method Time', readonly=True)
-
 
     def _get_asset_method_time(self):
         if self.env.context.get('active_id'):
@@ -55,10 +54,13 @@ class AssetModify(models.TransientModel):
             'method_period': self.method_period,
             'method_end': self.method_end,
         }
+        if asset_vals['method_number'] <= asset.entry_count:
+            raise UserError(_('The number of depreciations must be greater than the number of posted or draft entries '
+                              'to allow for complete depreciation of the asset.'))
         asset.write(asset_vals)
         asset.compute_depreciation_board()
         tracked_fields = self.env['account.asset.asset'].fields_get(['method_number', 'method_period', 'method_end'])
-        changes, tracking_value_ids = asset._message_track(tracked_fields, old_values)
+        changes, tracking_value_ids = asset._mail_track(tracked_fields, old_values)
         if changes:
             asset.message_post(subject=_('Depreciation board modified'), body=self.name, tracking_value_ids=tracking_value_ids)
         return {'type': 'ir.actions.act_window_close'}
